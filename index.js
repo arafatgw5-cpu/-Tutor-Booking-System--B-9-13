cc18ebe94883c51b8da8d813a81eac5abb5fbe51
Easin Arafat <arafatgw5@gmail.com>
2026-05-20 06:09:40 +0600

    Cors Added

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+// CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 // Middleware
 app.use(cors());
 app.use(express.json());
